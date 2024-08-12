668e383f1274d715efeceb54cf42130a69e7ad83
nehuencabibbo <ncabibbo@fi.uba.ar>
2024-08-12 02:44:28 -0300

    Finished 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,4 +6,20 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total += blog.likes, 0)
 }
 
-module.exports = {dummy, totalLikes}
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null;
+  
+  const mostLikedBlog = blogs.reduce((mostLikedBlog, blog) => 
+    (mostLikedBlog.likes || 0) > blog.likes ? mostLikedBlog : blog
+  , {})
+
+  const mostLikedBlogMapped = {
+    title: mostLikedBlog.title,
+    author: mostLikedBlog.author,
+    likes: mostLikedBlog.likes
+  }
+
+  return mostLikedBlogMapped
+}
+
+module.exports = {dummy, totalLikes, favoriteBlog}
