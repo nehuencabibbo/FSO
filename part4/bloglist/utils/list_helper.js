d34538088ccdc38b60a9e82fb12193ae42f00c99
nehuencabibbo <ncabibbo@fi.uba.ar>
2024-08-12 03:30:44 -0300

    Finished 4.6

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -8,7 +8,7 @@ const totalLikes = (blogs) => {
 
 const favoriteBlog = (blogs) => {
   if (blogs.length === 0) return null;
-  
+
   const mostLikedBlog = blogs.reduce((mostLikedBlog, blog) => 
     (mostLikedBlog.likes || 0) > blog.likes ? mostLikedBlog : blog
   , {})
@@ -22,4 +22,29 @@ const favoriteBlog = (blogs) => {
   return mostLikedBlogMapped
 }
 
-module.exports = {dummy, totalLikes, favoriteBlog}
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null
+  const authorBlogCount = new Map()
+
+  blogs.forEach((blog) => {
+    if (authorBlogCount.has(blog.author)) {
+      authorBlogCount.set(blog.author, authorBlogCount.get(blog.author) + 1)
+    } else {
+      authorBlogCount.set(blog.author, 1)
+    }
+  })
+
+  let authorWithTheMostBlogs = {}
+  for (const [author, blogs] of authorBlogCount.entries()) {
+    if (blogs > (authorWithTheMostBlogs.blogs || 0)) {
+      authorWithTheMostBlogs = {
+        author: author,
+        blogs: blogs
+      }
+    }
+  }
+
+  return authorWithTheMostBlogs
+}
+
+module.exports = {dummy, totalLikes, favoriteBlog, mostBlogs}
