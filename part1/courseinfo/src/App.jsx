19dcef384a504ad2496e48d2a2672a073b97bece
nehuencabibbo <ncabibbo@fi.uba.ar>
2024-12-29 03:03:46 -0300

    Finished 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,28 +3,30 @@ import Header from "./Header";
 import Total from "./Total";
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    }
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course}></Header>
-      <Content parts={parts}></Content>
+      <Header course={course.name}></Header>
+      <Content parts={course.parts}></Content>
       <Total
-        exercises={parts.map((partObject) => partObject.exercises)}
+        exercises={course.parts.map((partObject) => partObject.exercises)}
       ></Total>
     </div>
   );
