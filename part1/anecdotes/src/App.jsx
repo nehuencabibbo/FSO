8a9640cd7fc186deed350ea18824d70a8a87e89d
nehuencabibbo <ncabibbo@fi.uba.ar>
2024-12-30 04:56:42 -0300

    Finished 1.12

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,17 +1,5 @@
 import { useState } from 'react'
 
-const Title = ({text}) => (<h1>{text}</h1>)
-
-const Button = ({text, handleClick}) => {
-  return (
-    <button onClick={handleClick}>
-      {text}
-    </button>
-  )
-}
-
-const Anecdote = ({text}) => (<div>{text}</div>)
-
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -23,40 +11,26 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
-
-  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+   
   const [selected, setSelected] = useState(0)
 
-  const goToNextAnecdote = (amountOfAnecdotes) => {
+  const handleSelected = () => {
     const getRandomNumber = (min, max) => {
       return Math.floor(Math.random() * (max - min) + min);
     }
-  
-    setSelected(getRandomNumber(0, amountOfAnecdotes - 1))
+
+    setSelected(getRandomNumber(0, anecdotes.length - 1))
   }
-
-  const voteForSelectedAnecdote = () => {
-    const newVotes = votes.concat()
-    newVotes[selected] += 1
-
-    setVotes(newVotes)
-  }
-
-  const mostVotedAnecdote = () => {}
 
   return (
     <>
-      <Title text="Anecdote of the day"/>
-      <Anecdote text={anecdotes[selected]} />
-      <div>has {votes[selected]} votes</div>
-
-      <Button text="next anecdote" handleClick={() => goToNextAnecdote(anecdotes.length)}/>
-      <Button text="vote" handleClick={voteForSelectedAnecdote}/>
-
-      <Title text="Anecdote with most votes"/>
-      <Anecdote text= {anecdotes[votes.indexOf(Math.max(...votes))]} />
+      <button onClick={handleSelected}>Next anecdote</button>
+      <div>
+        {anecdotes[selected]}
+      </div>
     </>
   )
 }
 
 export default App
+
