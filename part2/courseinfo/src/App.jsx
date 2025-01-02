54253e6396ac6fd89abeb97d0311fc90b4f62673
nehuencabibbo <ncabibbo@fi.uba.ar>
2025-01-02 20:51:44 -0300

    Finished .2

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -5,11 +5,11 @@ const App = () => {
     id: 1,
     name: 'Half Stack application development',
     parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
       {
         name: 'Using props to pass data',
         exercises: 7,
--- part2/courseinfo/src/Course.jsx
+++ part2/courseinfo/src/Course.jsx
@@ -1,21 +1,25 @@
-const Header = ({ course }) => <h1>{course}</h1>
+const Header = ({ course }) => <h1>{course}</h1>;
 
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+const Total = ({ sum }) => <p style={{fontWeight: 'bold'}}>Number of exercises {sum}</p>;
 
-const Part = ({ part }) => 
+const Part = ({ part }) => (
   <p>
     {part.name} {part.exercises}
   </p>
+);
 
-const Content = ({ parts }) => parts.map(part => <Part part={part}/>)
+const Content = ({ parts }) => parts.map((part) => <Part part={part} />);
 
-const Course = ({course}) => {
-    return (
-        <>
-            <Header course={course.name}/>
-            <Content parts={course.parts}/>
-        </>
-    )
-}
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total
+        sum={course.parts.reduce((total, part) => (total += part.exercises), 0)}
+      />
+    </>
+  );
+};
 
-export default Course
+export default Course;
