b26ef6d626b305be4acc3a21266162d77f574c6c
nehuencabibbo <ncabibbo@fi.uba.ar>
2025-01-05 19:53:41 -0300

    Finished 2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,145 +1,38 @@
-import { useState, useEffect } from 'react'
-import Filter from './components/Filter'
-import ContactData from './components/ContactData'
-import NumberList from './components/NumberList'
-import personService from './services/person'
-import Notification from './components/Notification'
-import './index.css'
-
-const Title = ({text}) => (<h1>{text}</h1>)
-
-const SubTitle = ({text}) => (<h2>{text}</h2>)
+import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([]) 
-  const [newName, setNewName] = useState('Martin Fowler')
-  const [newNumber, setNewNumber] = useState('15-4112-3807')
-  const [filter, setFilter] = useState('')
-  const [operationCompletedMessage, setOperationCompletedMessage] = useState(null)
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  console.log(newName)
 
-  useEffect(() => {
-    personService
-      .getAll()
-      .then(returnedPersons => {
-        setPersons(returnedPersons)
-      })
-  }, [])
-
-  const addPerson = (event) => {
+  const handleNewPerson = (event) => {
     event.preventDefault()
 
-    const newPerson = {name: newName, number: newNumber}
+    const newPersons = persons.concat({name: newName})
+    setPersons(newPersons)
+    setNewName('')
+  }
 
-    const isPersonInPhonebook = (persons, newPerson) => {
-      return persons.some(
-        (person) => 
-          person.name == newPerson.name)
-    }
-
-    if (!isPersonInPhonebook(persons, newPerson)) {
-      personService
-        .add(newPerson)
-        .then(returnedPerson => { 
-          setPersons(persons.concat(returnedPerson))
-
-          setOperationCompletedMessage(
-            {
-              message: `Added ${returnedPerson.name}`,
-              type: 'success',
-            }
-          )
-          setTimeout(() => {
-            setOperationCompletedMessage(null)
-          }, 5000)
-        })
-    } else {
-      const confirmationText = `${newPerson.name} is already added to phonebook, replace the old number with a new one?`
-      if (confirm(confirmationText)){
-        const newPersonId = persons
-          .find(person => 
-            person.name == newPerson.name)
-          .id
-        
-        personService
-          .change(newPersonId, newPerson)
-          .then(personWithNewNumber => {
-            setPersons(persons.map(
-              person =>
-                (person.id == personWithNewNumber.id) 
-                ? personWithNewNumber
-                : person
-              )
-            )
-            setOperationCompletedMessage(
-              {
-                message: `${newPerson.name}'s number was changed to ${newPerson.number}`, 
-                type: 'success',
-              })
-            setTimeout(() => {
-              setOperationCompletedMessage(null)
-              }, 20000)
-            }
-          )
-          .catch(error => {
-            //TODO: Fetchear todo denuevo
-            setOperationCompletedMessage(
-              {
-              message: `Couldn't change ${newPerson.name}'s phone number, person was previously deleted from the server`,
-              type: 'error'
-              }
-            )
-            setTimeout(() => {
-              setOperationCompletedMessage(null)
-            }, 5000)
-          })
-
-          setPersons(
-              persons
-                  .filter(person => 
-                      newPersonId != person.id))  
-                  
-      }
-      }
-    }
-
+  const handleNewPersonChange = (event) => {
+    setNewName(event.target.value)
+  }
   return (
     <div>
-      <Title text={"Phonebook"}/>
-      <Notification 
-        message={
-          operationCompletedMessage != null ? operationCompletedMessage.message : null
-        } 
-        type={
-          operationCompletedMessage != null ? operationCompletedMessage.type : null
-        }
-      />
-
-      <SubTitle text={"Filter"}/>
-      <Filter 
-        filterValue={filter} 
-        handleFilterChange={
-          (event) => setFilter(event.target.value)}/>
-      
-      <SubTitle text={"Contact data"}/>
-      <ContactData 
-        addPerson={addPerson} 
-        newName={newName} 
-        newNumber={newNumber}
-        handleNewName={
-          (event) => {
-            setNewName(event.target.value)
-        }}
-        handleNewNumber={
-          (event) => {
-            setNewNumber(event.target.value)
-          }
-        }/>
-
-      <SubTitle text={"Numbers"}/>
-      <NumberList 
-        persons={persons} 
-        filter={filter}
-        setPersons={setPersons}/>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleNewPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNewPersonChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => <li key={person.name}>{person.name}</li>)}
+      </ul>
     </div>
   )
 }
--- part2/phonebook/src/components/Filter.jsx
+++
@@ -1,7 +0,0 @@
-const Filter = ({filterValue, handleFilterChange}) => (
-    <div>
-        Only show numbers that contain: <input value={filterValue} onChange={handleFilterChange}/>
-    </div>
-)
-
-export default Filter
